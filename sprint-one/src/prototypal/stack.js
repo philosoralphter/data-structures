c34b21658250434d3bff44d62f3bb0d2a7cd4a66
philosoralphter <theralphsamuel@gmail.com>
2014-09-10 20:58:44 -0700

    Mastered prototypal for Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,31 +1,34 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  var stackInstance = Object.create(makeStack.prototype);
+
+  var stackInstance = Object.create(stackMethods);
   stackInstance.sizzle = 0;
-  stackInstance.storage ={};
+  stackInstance.storage = {};
 
   return stackInstance;
 };
 
-makeStack.prototype.push = function(value){
-  this.storage[this.sizzle] = value;
-  this.sizzle++;
-};
+var stackMethods = {
+  push : function(value){
+    this.storage[this.sizzle] = value;
+    this.sizzle++; },
 
-makeStack.prototype.pop = function(){
-  if (this.sizzle>0){
-    var result = this.storage[this.sizzle-1];
-    this.sizzle--;
-    return result;
-  }else{
-    return null;
+    pop: function(){
+      if (this.sizzle>0){
+        var result = this.storage[this.sizzle-1];
+        this.sizzle--;
+        return result;
+      }else{
+        return null;
+      }
+    },
+
+    size: function(){
+      return this.sizzle;
+
+    }
   }
-};
 
-makeStack.prototype.size = function(){
-  return this.sizzle;
-};
+
 
 
 
