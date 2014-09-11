b349d36c2be0a26e4615eca957c992d49b1b9c40
philosoralphter <theralphsamuel@gmail.com>
2014-09-11 14:27:30 -0700

    linkedList completed. began tree.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,37 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var aNode = makeNode(value);
+
+    if (!this.head) {
+      this.head = aNode;
+      this.tail = aNode;
+    } else {
+      this.tail.next = aNode;
+      this.tail = aNode;
+    }
   };
 
   list.removeHead = function(){
+    var result = this.head;
+    this.head = this.head.next;
+    return result.value;
   };
 
   list.contains = function(target){
-  };
+    var current = this.head;
+
+    while (current != null) {
+      if (current.value == target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
 
   return list;
+
 };
 
 var makeNode = function(value){
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
@@ -11,13 +11,17 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) {
+    return true;
+  } else{
+    //for loop on children
+      //return eachChild.contains(target)
+  }
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
