6adbf04a0c68075c1b40449b02d1e8d1fd646997
philosoralphter <theralphsamuel@gmail.com>
2014-09-12 16:43:33 -0700

    Almost finished with Hash table EC

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,28 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
 };
 
+
 HashTable.prototype.insert = function(key, value){
+
   var location = getIndexBelowMaxForKey(key, this._limit);
   var pair = [key, value];
   var element = this._storage.get(location);
 
   if (element === undefined){
     this._storage.set(location, [pair]);
+    this._size++;
   }else{
     element.push(pair);
     this._storage.set(location, element);
+    this._size++;
   }
+
+
+  this.sizeCheck();
+
 };
 
 HashTable.prototype.retrieve = function(key){
@@ -35,16 +44,48 @@ HashTable.prototype.remove = function(key){
     if (element[i][0] === key){
       element[i][1] = null;
       this._storage.set(location, element)
+      this.size--;
     }
   }
 };
 
-/*HashTable.prototype.double = function() {
-  this._limit = this._limit*2;
-  var tempstorage = this._storage;
-  this._storage = makeLimitedArray(this._limit);
-  tempstoraged.each(callback involves grabbing tempstorage and putting it into each)
-}*/
+HashTable.prototype.changeSize = function(todo) {
+
+  if (todo === 'double') {
+    var tempStorage = this._storage;
+    this._limit *= 2;
+    this._storage = makeLimitedArray(this._limit);
+
+  }else if (todo ==='halve'){
+    var tempStorage = this._storage;
+    this._limit /= 2;
+    this._storage = makeLimitedArray(this._limit);
+  }
+
+  var transfer = function(element, index, storage) {
+    for (var i=0; i<element.length; i++){
+      var currentPair = element[i];
+      this.insert(currentPair[0], currentPair[1]);
+    }
+  }
+
+  tempStorage.each(transfer);
+};
+
+
+HashTable.prototype.sizeCheck = function(){
+  console.log(this._size)
+
+  if (this._size > this._limit) {
+   console.log("doubling table size");
+   this.changeSize('double');
+
+  }else if (this._size < this._limit/2 && this._limit > 8){
+    console.log("halving size");
+    this.changeSize('halve');
+  }
+
+};
 
 
 /*
